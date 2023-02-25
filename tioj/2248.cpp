05e6ce249f687273faa921a8d6985b30d80f7db5
ET01 <kagariet01@outlook.com>
2023-02-25 13:52:17 +0800

    AC tioj 2248

--- tioj/2248.cpp
+++ tioj/2248.cpp
@@ -1,6 +1,7 @@
 /*
-[zj]				[Q]https://zerojudge.tw/ShowProblem?problemid=b115
-[]
+[tioj]			[Q]https://tioj.ck.tp.edu.tw/problems/2248
+[DFS]
+[AC]
 */
 
 
@@ -51,57 +52,33 @@ using namespace std;
 /*num*/
 bool debug=0;
 bool iofast=true;
-PII mv[]={{0,1},{1,0},{0,-1},{-1,0}};
-INT mx[]={0,1,0,-1};
-INT my[]={1,0,-1,0};
-INT mod=988244353;
 /*fn定義*/
 template<typename TPE>TPE reader(){
 	TPE a;
 	cin>>a;
 	return a;
 }
-struct bignum{
-	vector<INT> vec;
-	string s="";
-	INT sz=0;
-	void build(){
-		vec.clear();
-		sz=s.size();
-		vec.resize(sz);
-		for(INT i=0;i<sz;i++){
-			vec[i]=s[i]-'0';
-		}
-	}
-	INT size(){
-		return sz;
-	}
-	void inin(){
-		sz=vec.size()+1;
-		vec.resize(sz);
-		for(INT i=0;i<sz;i++){
-			vec[i+1]+=vec[i]/10;
-			vec[i]%=10;
-		}
-	}
-};
 
-bignum operator*(const bignum &a,const bignum &b){
-	bignum re;
-	re.sz=a.sz+b.sz;
-	re.vec.resize(re.sz);
-	for(INT i=0;i<a.sz;i++){
-		if(a.vec[i]==0)continue;
-		for(INT j=0;j<b.sz;j++){
-			re.vec[i+j]+=a.vec[i]*i+b.vec[i]*j;
-		}
+const INT mxn=1e5;
+INT w[mxn+1];
+vector<INT> vec[mxn+1];
+map<PII,INT> mp;
+INT ans=0;
+INT n,k;
+
+void dfs(INT n,INT lst=-1){
+	//cerr<<"w["<<n<<"]="<<w[n]<<endl;
+	for(INT i:vec[n]){
+		if(lst==i)continue;
+		//cerr<<n<<">"<<i<<endl;
+		dfs(i,n);
+		w[n]+=w[i]-k;
+		ans+=abs(w[i]-k)*mp[{n,i}];
+		w[i]=k;
 	}
-	return re;
 }
-bignum operator/(const bignum &a,const bignum &b){
-	bignum re;
-	return re;
-}
+
+
 
 
 
@@ -110,18 +87,56 @@ bignum operator/(const bignum &a,const bignum &b){
 int main(){
 	if(!debug&&iofast){what_the_fuck;}
 	srand(time(NULL));
-	string sa;
-	while(cin>>sa){
-
+	INT t=1;
+	while(t--){
+		set0(w);
+		for(INT i=0;i<mxn;i++){
+			vec[i].clear();
+		}
+		ans=0;
+		mp.clear();
 		/*CIN*/
+		cin>>n>>k;
+		for(INT i=0;i<n;i++){
+			cin>>w[i];
+		}
+		for(INT i=0;i<n-1;i++){
+			INT u,v,d;
+			cin>>u>>v>>d;
+			u--;
+			v--;
+			vec[u].push_back(v);
+			vec[v].push_back(u);
+			mp[{u,v}]=mp[{v,u}]=d;
+		}
 		/*solve*/
+		dfs(0);
+		cout<<ans<<endl;
 	}
 	return 0;
 }
 
 /*
 [I1]
+8 2
+4 2 2 1 3 3 0 1
+1 2 3
+2 3 1
+3 4 2
+5 6 2
+6 7 3
+6 8 1
+2 6 3
 [O1]
+21
+[I2]
+4 3
+1 10 0 1
+1 4 3
+3 2 2
+4 2 1
+[O2]
+16
 */
 
 /*think*/
