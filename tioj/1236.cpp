0c5ec417f27b803e9173c285d7ce4c448cfc952d
ET01 <66681962+KagariET01@users.noreply.github.com>
2023-03-02 19:53:19 +0800

    AC tioj 1235

---
+++ tioj/1236.cpp
@@ -0,0 +1,111 @@
+/*
+[tioj]			[Q]https://tioj.ck.tp.edu.tw/problems/ [ID]
+[zj]				[Q]https://zerojudge.tw/ShowProblem?problemid= [ID]
+[cses]			[Q]https://cses.fi/problemset/task/ [ID]
+[AtCoder]		[Q]https://atcoder.jp/contests/ [ID] /tasks/ [ID] _ [PID]
+[CF]				[Q]
+[ioic_2023]	[Q]https://judge.ioicamp.org/problems/ [ID]
+[]
+*/
+
+
+/*include*/
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O2")
+#pragma GCC optimize("O1")
+#pragma GCC optimize("O3")
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<map>
+#include<set>
+#include<cstring>
+#include<iomanip>
+#include<ctime>
+/*using namespace*/
+using namespace std;
+/*define type*/
+#define what_the_fuck cin.tie(0);cout.tie(0);ios::sync_with_stdio(false)
+#define ULLI unsigned long long int
+#define LLI long long int
+#define INT LLI
+#define UINT unsigned INT
+#define PII pair<INT,INT>
+#define PUIUI pair<UINT,UINT>
+#define endl "\n"
+#define DBG if(debug)
+#define FIR first
+#define SEC second
+#define elif else if
+#define wassomething() empty()==false
+#define REre return re
+#define P(n,m) pair<n,m>
+#define read(n) reader<n>()
+#define set0(n) memset(n,0,sizeof(n))
+#define Aloop(i,s,n,ad) for(INT i=s;i<n;i+=ad)
+#define loop(i,s,n) for(INT i=s;i<n;i++)
+#define Dloop(i,s,n) for(i=s;i<n;i++)
+#define ADloop(i,s,n,ad) for(i=s;i<n;i+=ad)
+/*num*/
+bool debug=0;
+bool iofast=true;
+PII mv[]={{0,1},{1,0},{0,-1},{-1,0}};
+INT mx[]={0,1,0,-1};
+INT my[]={1,0,-1,0};
+INT mod=988244353;
+/*fn定義*/
+template<typename TPE>TPE reader(){
+	TPE a;
+	cin>>a;
+	return a;
+}
+
+const INT mxn=100;
+INT wk[mxn+1];
+int n,m;
+vector<INT> tre[mxn+1];
+bool isst[mxn+1];
+
+
+
+
+
+/*main*/
+int main(){
+	if(!debug&&iofast){what_the_fuck;}
+	srand(time(NULL));
+	INT t=1;
+	while(t--){
+    memset(isst,true,sizeof(isst));
+		/*CIN*/
+		n=read(INT);
+		for(INT i=1;i<=n;i++){
+      wk[i]=read(INT);
+    }
+    m=read(INT);
+    for(INT i=1;i<=m;i++){
+      int a,b;cin>>a>>b;
+      tre[a].push_back(b);
+    }
+    /*solve*/
+
+	}
+	return 0;
+}
+
+/*
+[I1]
+[O1]
+*/
+
+/*think*/
+/*
+
+*/
